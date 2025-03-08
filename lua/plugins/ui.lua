d24b4b9aa67eb37b3c6628e890181d9a1922d47f
w1sent <w1sent>
2025-03-08 20:12:49 +0100

    clean up and update key mappings

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -20,14 +20,6 @@ return {
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
       require("telescope").setup()
-
-      -- set keymaps
-      local keymap = vim.keymap
-      keymap.set("n", "<leader>f", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files" })
-      keymap.set("v", "<leader>fg", "<cmd>Telescope grep_string<cr>", { desc = "Find string in files" })
-      keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { desc = "Live grep search" })
-      keymap.set("n", "<leader>fR", "<cmd>Telescope lsp_references<cr>", { desc = "references for symbol" })
-      keymap.set("v", "<leader>t", "<cmd>Telescope<cr>", { desc = "Telescope Commands" })
     end,
   },
   {
@@ -49,65 +41,10 @@ return {
     },
   },
   {
-    "folke/trouble.nvim",
-    cmd = { "Trouble" },
-    opts = {
-      modes = {
-        lsp = {
-          win = { position = "right" },
-        },
-      },
-    },
-    keys = {
-      { "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>",              desc = "Diagnostics (Trouble)" },
-      { "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Buffer Diagnostics (Trouble)" },
-      { "<leader>cs", "<cmd>Trouble symbols toggle<cr>",                  desc = "Symbols (Trouble)" },
-      { "<leader>cS", "<cmd>Trouble lsp toggle<cr>",                      desc = "LSP references/definitions/... (Trouble)" },
-      { "<leader>xL", "<cmd>Trouble loclist toggle<cr>",                  desc = "Location List (Trouble)" },
-      { "<leader>xQ", "<cmd>Trouble qflist toggle<cr>",                   desc = "Quickfix List (Trouble)" },
-      {
-        "[q",
-        function()
-          if require("trouble").is_open() then
-            require("trouble").prev({ skip_groups = true, jump = true })
-          else
-            local ok, err = pcall(vim.cmd.cprev)
-            if not ok then
-              vim.notify(err, vim.log.levels.ERROR)
-            end
-          end
-        end,
-        desc = "Previous Trouble/Quickfix Item",
-      },
-      {
-        "]q",
-        function()
-          if require("trouble").is_open() then
-            require("trouble").next({ skip_groups = true, jump = true })
-          else
-            local ok, err = pcall(vim.cmd.cnext)
-            if not ok then
-              vim.notify(err, vim.log.levels.ERROR)
-            end
-          end
-        end,
-        desc = "Next Trouble/Quickfix Item",
-      },
-    },
-  },
-  {
     "folke/todo-comments.nvim",
     cmd = { "TodoTrouble", "TodoTelescope" },
     opts = {},
     -- stylua: ignore
-    keys = {
-      { "]t",         function() require("todo-comments").jump_next() end,              desc = "Next Todo Comment" },
-      { "[t",         function() require("todo-comments").jump_prev() end,              desc = "Previous Todo Comment" },
-      { "<leader>xt", "<cmd>Trouble todo toggle<cr>",                                   desc = "Todo (Trouble)" },
-      { "<leader>xT", "<cmd>Trouble todo toggle filter = {tag = {TODO,FIX,FIXME}}<cr>", desc = "Todo/Fix/Fixme (Trouble)" },
-      { "<leader>st", "<cmd>TodoTelescope<cr>",                                         desc = "Todo" },
-      { "<leader>sT", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>",                 desc = "Todo/Fix/Fixme" },
-    },
   },
 
   { -- show images in nvim!
@@ -154,65 +91,21 @@ return {
         max_height_window_percentage = 30,
         kitty_method = 'normal',
       }
-
-      local function clear_all_images()
-        local bufnr = vim.api.nvim_get_current_buf()
-        local images = image.get_images { buffer = bufnr }
-        for _, img in ipairs(images) do
-          img:clear()
-        end
-      end
-
-      local function get_image_at_cursor(buf)
-        local images = image.get_images { buffer = buf }
-        local row = vim.api.nvim_win_get_cursor(0)[1] - 1
-        for _, img in ipairs(images) do
-          if img.geometry ~= nil and img.geometry.y == row then
-            local og_max_height = img.global_state.options.max_height_window_percentage
-            img.global_state.options.max_height_window_percentage = nil
-            return img, og_max_height
-          end
-        end
-        return nil
-      end
-
-      local create_preview_window = function(img, og_max_height)
-        local buf = vim.api.nvim_create_buf(false, true)
-        local win_width = vim.api.nvim_get_option_value('columns', {})
-        local win_height = vim.api.nvim_get_option_value('lines', {})
-        local win = vim.api.nvim_open_win(buf, true, {
-          relative = 'editor',
-          style = 'minimal',
-          width = win_width,
-          height = win_height,
-          row = 0,
-          col = 0,
-          zindex = 1000,
-        })
-        vim.keymap.set('n', 'q', function()
-          vim.api.nvim_win_close(win, true)
-          img.global_state.options.max_height_window_percentage = og_max_height
-        end, { buffer = buf })
-        return { buf = buf, win = win }
-      end
-
-      local handle_zoom = function(bufnr)
-        local img, og_max_height = get_image_at_cursor(bufnr)
-        if img == nil then
-          return
-        end
-
-        local preview = create_preview_window(img, og_max_height)
-        image.hijack_buffer(img.path, preview.win, preview.buf)
-      end
-
-      vim.keymap.set('n', '<leader>io', function()
-        local bufnr = vim.api.nvim_get_current_buf()
-        handle_zoom(bufnr)
-      end, { buffer = true, desc = 'image [o]pen' })
-
-      vim.keymap.set('n', '<leader>ic', clear_all_images, { desc = 'image [c]lear' })
     end,
   },
-
+  {
+    'echasnovski/mini.move',
+    version = '*'
+  },
+  {
+    'stevearc/oil.nvim',
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {},
+    -- Optional dependencies
+    dependencies = { { "echasnovski/mini.icons", opts = {} } },
+    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+    lazy = false,
+  }
 }
--- lua/settings/mapping.lua
+++ lua/settings/mapping.lua
@@ -1,64 +1,9 @@
 local map = vim.keymap.set
 local wk = require 'which-key'
 
-local nmap = function(key, effect)
-  vim.keymap.set('n', key, effect, { silent = true, noremap = true })
-end
-
-local vmap = function(key, effect)
-  vim.keymap.set('v', key, effect, { silent = true, noremap = true })
-end
-
-local imap = function(key, effect)
-  vim.keymap.set('i', key, effect, { silent = true, noremap = true })
-end
-
-local cmap = function(key, effect)
-  vim.keymap.set('c', key, effect, { silent = true, noremap = true })
-end
-
-
-map("n", "<leader>q", ":q!<CR>", {})
-map("n", "<leader>s", ":w<CR>", {})
-
--- Window managment
-map("n", "<leader>wh", "<C-w>h", { desc = "switch window left" })
-map("n", "<leader>wj", "<C-w>l", { desc = "switch window right" })
-map("n", "<leader>wk", "<C-w>k", { desc = "switch window up" })
-map("n", "<leader>wl", "<C-w>j", { desc = "switch window down" })
-map("n", "<leader>w<left>", "<C-w>h", { desc = "switch window left" })
-map("n", "<leader>w<right>", "<C-w>l", { desc = "switch window right" })
-map("n", "<leader>w<up>", "<C-w>k", { desc = "switch window up" })
-map("n", "<leader>w<down>", "<C-w>j", { desc = "switch window down" })
-map("n", "<leader>wn", "<C-w>n", { desc = "create window horizontal" })
-map("n", "<leader>wv", "<C-w>v", { desc = "create window vertical" })
-
--- Comment
-map("n", "<leader>c", "gcc", { desc = "Toggle comment", remap = true })
-map("v", "<leader>c", "gc", { desc = "Toggle comment", remap = true })
-
--- LSP-Functions
-map("n", "<leader>D", ":lua vim.lsp.buf.declaration()<CR>", { desc = "Jump to declaration", remap = true })
-map("n", "<leader>d", ":lua vim.lsp.buf.definition()<CR>", { desc = "Jump to definition", remap = true })
-map("n", "<leader>k", ":lua vim.lsp.buf.hover()<CR>", { desc = "Show documentation", remap = true })
-map("n", "<leader>a", ":lua vim.lsp.buf.code_action()<CR>", { desc = "perform code action", remap = true })
-map("n", "<leader>r", ":lua vim.lsp.buf.rename()<CR>", { desc = "rename symbol", remap = true })
-map("n", "<leader>F", ":lua conform.format()<CR>", { desc = "Format document", remap = true })
-
--- Terminal
-function _G.set_terminal_keymaps()
-  local opts = { buffer = 0 }
-  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
-  vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
-  vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
-  vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
-  vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
-  vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
-  vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
-end
-
--- if you only want these mappings for toggle term use term://*toggleterm#* instead
-vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+-- ############################################
+-- Helper functions
+-- ############################################
 
 --- Send code to terminal with vim-slime
 --- If an R terminal has been opend, this is in r_mode
@@ -114,37 +59,6 @@ local function send_region()
   end
 end
 
--- send code with ctrl+Enter
--- just like in e.g. RStudio
--- needs kitty (or other terminal) config:
--- map shift+enter send_text all \x1b[13;2u
--- map ctrl+enter send_text all \x1b[13;5u
-nmap('<c-cr>', send_cell)
-nmap('<s-cr>', send_cell)
-imap('<c-cr>', send_cell)
-imap('<s-cr>', send_cell)
-
---- Show R dataframe in the browser
--- might not use what you think should be your default web browser
--- because it is a plain html file, not a link
--- see https://askubuntu.com/a/864698 for places to look for
-local function show_r_table()
-  local node = vim.treesitter.get_node { ignore_injections = false }
-  assert(node, 'no symbol found under cursor')
-  local text = vim.treesitter.get_node_text(node, 0)
-  local cmd = [[call slime#send("DT::datatable(]] .. text .. [[)" . "\r")]]
-  vim.cmd(cmd)
-end
-
--- keep selection after indent/dedent
-vmap('>', '>gv')
-vmap('<', '<gv')
-
--- center after search and jumps
-nmap('n', 'nzz')
-nmap('<c-d>', '<c-d>zz')
-nmap('<c-u>', '<c-u>zz')
-
 --- Insert code chunk of given language
 --- Splits current chunk if already within a chunk
 --- @param lang string
@@ -185,58 +99,243 @@ local function new_terminal_shell()
   new_terminal '$SHELL'
 end
 
+
+-- ############################################
+-- Mappings
+-- ############################################
+map("n", "<leader>q", ":q!<CR>", { desc = "force quit" })
+map("n", "<leader>s", ":w<CR>", { desc = "save" })
+
+-- Window managment
+map("n", "<leader>wh", "<C-w>h", { desc = "switch window left" })
+map("n", "<leader>wj", "<C-w>l", { desc = "switch window right" })
+map("n", "<leader>wk", "<C-w>k", { desc = "switch window up" })
+map("n", "<leader>wl", "<C-w>j", { desc = "switch window down" })
+map("n", "<leader>w<left>", "<C-w>h", { desc = "switch window left" })
+map("n", "<leader>w<right>", "<C-w>l", { desc = "switch window right" })
+map("n", "<leader>w<up>", "<C-w>k", { desc = "switch window up" })
+map("n", "<leader>w<down>", "<C-w>j", { desc = "switch window down" })
+map("n", "<leader>wn", "<C-w>n", { desc = "create window horizontal" })
+map("n", "<leader>wv", "<C-w>v", { desc = "create window vertical" })
+
+-- Comment
+map("n", "<leader>cc", "gcc", { desc = "Toggle comment", remap = true })
+map("v", "<leader>cc", "gc", { desc = "Toggle comment", remap = true })
+
+-- Code mappings
+map("n", "<leader>c<cr>", send_cell, { desc = "Send code cell" })
+map("v", "<leader>c<cr>", send_region, { desc = "Send code region" })
+
+map("n", "<leader>ctp", new_terminal_python, { desc = "New python terminal" })
+map("n", "<leader>cti", new_terminal_ipython, { desc = "New ipython terminal" })
+map("n", "<leader>cts", new_terminal_shell, { desc = "New shell terminal" })
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
+  callback = function(event)
+    local function lspmap(keys, func, desc)
+      vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+    end
+    local function lspvmap(keys, func, desc)
+      vim.keymap.set('v', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+    end
+    local client = vim.lsp.get_client_by_id(event.data.client_id)
+    assert(client, 'LSP client not found')
+
+    ---@diagnostic disable-next-line: inject-field
+    client.server_capabilities.document_formatting = true
+
+    lspmap('<leader>cS', vim.lsp.buf.document_symbol, 'go so symbols')
+    lspmap('<leader>cT', vim.lsp.buf.type_definition, 'go to type definition')
+    lspmap('<leader>ct', vim.lsp.buf.definition, 'go to definition')
+    lspmap('<leader>ck', vim.lsp.buf.hover, 'hover documentation')
+    lspmap('<leader>ch', vim.lsp.buf.signature_help, 'go to signature help')
+    lspmap('<leader>ci', vim.lsp.buf.implementation, 'go to Implementation')
+    lspmap('<leader>cdp', function() vim.diagnostic.jump({ count = 1 }) end, 'previous diagnostic ')
+    lspmap('<leader>cdn', function() vim.diagnostic.jump({ count = -1 }) end, 'next diagnostic ')
+    lspmap('<leader>cl', vim.lsp.codelens.run, 'lens run')
+    lspmap('<leader>cr', vim.lsp.buf.rename, 'lsp rename')
+    lspmap('<leader>cf', vim.lsp.buf.format, 'lsp format')
+    lspvmap('<leader>cf', vim.lsp.buf.format, 'lsp format')
+    lspmap('<leader>cq', vim.diagnostic.setqflist, 'lsp diagnostic quickfix')
+    lspmap("<leader>cdd", function() vim.diagnostic.enable(false) end, "Disable diagnostics")
+    lspmap("<leader>cde", vim.diagnostic.enable, "Enable diagnostics")
+    lspmap("<leader>ce", vim.diagnostic.open_float, "Diagnostics (show hover error)")
+
+    lspmap("<leader>cD", "<cmd>Telescope diagnostics<cr>", "Pick diagnostics")
+    lspmap("<leader>cS", "<cmd>Telescope lsp_document_symbols<cr>", "Pick symbols is document")
+    lspmap("<leader>cR", "<cmd>Telescope lsp_references<cr>", "Pick references")
+    lspmap("<leader>ca", "<cmd>Telescope lsp_code_actions<cr>", "Pick code actions")
+    lspmap("<leader>cI", "<cmd>Telescope lsp_incoming_calls<cr>", "Pick incoming calls")
+    lspmap("<leader>cO", "<cmd>Telescope lsp_outgoing_calls<cr>", "Pick outgoing calls")
+  end,
+})
+
+
+-- Telescope mappings
+map("n", "<leader>v", "<cmd>Telescope find_files<cr>", { desc = "Find files" })
+map("n", "<leader>g", "<cmd>Telescope live_grep<cr>", { desc = "Find string in files" })
+map("n", "<leader>fv", "<cmd>Telescope find_files<cr>", { desc = "Find files" })
+map("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { desc = "Find string in files" })
+map("n", "<leader>fb", "<cmd>Telescope current_buffer_fuzzy_find<cr>", { desc = "Find in buffer" })
+map("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", { desc = "Find help tags" })
+map("n", "<leader>fM", "<cmd>Telescope marks<cr>", { desc = "Find marks" })
+map("n", "<leader>fl", "<cmd>Telescope loclist<cr>", { desc = "Find loclist" })
+map("n", "<leader>fq", "<cmd>Telescope quickfix<cr>", { desc = "Find quickfix" })
+map("n", "<leader>fr", "<cmd>Telescope resume<cr>", { desc = "Resume last telescope" })
+map("n", "<leader>ft", "<cmd>Telescope treesitter<cr>", { desc = "Find treesitter" })
+map("n", "<leader>fG", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor" })
+map("n", "<leader>fc", "<cmd>Telescope commands <cr>", { desc = "Pick command" })
+map("n", "<leader>fm", "<cmd>Telescope man_pages<cr>", { desc = "Find manpage" })
+map("n", "<leader>fs", "<cmd>Telescope spell_suggest<cr>", { desc = "Find spelling suggestions" })
+map("v", "<leader>fg", "<cmd>Telescope grep_string<cr>", { desc = "Find string in files" })
+map("v", "<leader>fC", "<cmd>Telescope<cr>", { desc = "Telescope Commands" })
+
+
+-- help mappings
+map("n", "<leader>hch", ":set conceallevel=1<CR>", { desc = "Hide conceal" })
+map("n", "<leader>hcs", ":set conceallevel=0<CR>", { desc = "Show conceal" })
+map("n", "<leader>ht", vim.treesitter.inspect_tree, { desc = "Inspect treesitter" })
+map("n", "<leader>d", "<cmd>vsplit<cr> <cmd>Oil<cr>", { desc = "Oil: edit filesystem" })
+
+
+-- Terminal
+function _G.set_terminal_keymaps()
+  local opts = { buffer = 0 }
+  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+  vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+  vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+  vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+  vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+end
+
+-- if you only want these mappings for toggle term use term://*toggleterm#* instead
+vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+
+-- send code with ctrl+Enter
+-- just like in e.g. RStudio
+-- needs kitty (or other terminal) config:
+-- map shift+enter send_text all \x1b[13;2u
+-- map ctrl+enter send_text all \x1b[13;5u
+map("n", "<c-cr>", send_cell, { desc = "run code cell" })
+map("n", "<s-cr>", send_cell, { desc = "run code cell" })
+map("i", "<c-cr>", send_cell, { desc = "run code cell" })
+map("i", "<s-cr>", send_cell, { desc = "run code cell" })
+
+-- keep selection after indent/dedent
+map("v", "<", "<gv")
+map("v", ">", ">gv")
+
+-- center after search and jumps
+map("n", 'n', 'nzz')
+map("n", '<c-d>', '<c-d>zz')
+map("n", '<c-u>', '<c-u>zz')
+
+
+-- image.nvim mappings
+local image = require 'image'
+local function clear_all_images()
+  local bufnr = vim.api.nvim_get_current_buf()
+  local images = image.get_images { buffer = bufnr }
+  for _, img in ipairs(images) do
+    img:clear()
+  end
+end
+
+local function get_image_at_cursor(buf)
+  local images = image.get_images { buffer = buf }
+  local row = vim.api.nvim_win_get_cursor(0)[1] - 1
+  for _, img in ipairs(images) do
+    if img.geometry ~= nil and img.geometry.y == row then
+      local og_max_height = img.global_state.options.max_height_window_percentage
+      img.global_state.options.max_height_window_percentage = nil
+      return img, og_max_height
+    end
+  end
+  return nil
+end
+
+local create_preview_window = function(img, og_max_height)
+  local buf = vim.api.nvim_create_buf(false, true)
+  local win_width = vim.api.nvim_get_option_value('columns', {})
+  local win_height = vim.api.nvim_get_option_value('lines', {})
+  local win = vim.api.nvim_open_win(buf, true, {
+    relative = 'editor',
+    style = 'minimal',
+    width = win_width,
+    height = win_height,
+    row = 0,
+    col = 0,
+    zindex = 1000,
+  })
+  vim.keymap.set('n', 'q', function()
+    vim.api.nvim_win_close(win, true)
+    img.global_state.options.max_height_window_percentage = og_max_height
+  end, { buffer = buf })
+  return { buf = buf, win = win }
+end
+
+local handle_zoom = function(bufnr)
+  local img, og_max_height = get_image_at_cursor(bufnr)
+  if img == nil then
+    return
+  end
+
+  local preview = create_preview_window(img, og_max_height)
+  image.hijack_buffer(img.path, preview.win, preview.buf)
+end
+
+map('n', '<leader>io', function()
+  local bufnr = vim.api.nvim_get_current_buf()
+  handle_zoom(bufnr)
+end, { buffer = true, desc = 'image open' })
+map('n', '<leader>ic', clear_all_images, { desc = 'image [c]lear' })
+
+
+-- notebook mappings
+
+map("n", "<leader>nE", function() require('otter').export(true) end, { desc = "Export quarto with overwrite" })
+map("n", "<leader>na", require 'otter'.activate, { desc = "Activate otter" })
+map("n", "<leader>nb", insert_bash_chunk, { desc = "Insert bash chunk" })
+map("n", "<leader>nd", require 'otter'.activate, { desc = "Deactivate otter" })
+map("n", "<leader>ne", require('otter').export, { desc = "Export quarto" })
+map("n", "<leader>nh", ":QuartoHelp ", { desc = "Quarto help" })
+map("n", "<leader>np", insert_py_chunk, { desc = "Insert python chunk" })
+map("n", "<leader>nq", ":QuartoActivate<cr>", { desc = "Activate quarto" })
+map("n", "<leader>nra", ":QuartoSendAll<cr>", { desc = "Quarto run all" })
+map("n", "<leader>nrb", ":QuartoSendBelow<cr>", { desc = "Quarto run below" })
+map("n", "<leader>nrr", ":QuartoSendAbove<cr>", { desc = "Quarto run above" })
+map("n", "<leader>nrp", ":lua require'quarto'.quartoPreview()<cr>", { desc = "Preview quarto" })
+map("n", "<leader>nrq", ":lua require'quarto'.quartoClosePreview()<cr>", { desc = "Quiet preview quarto" })
+map("n", "<leader>nmi", ":MoltenInit<cr>", { desc = "Molten init" })
+map("v", "<leader>nmv", ":<C-u>MoltenEvaluateVisual<cr>", { desc = "Molten eval visual" })
+map("n", "<leader>nmr", ":MoltenReevaluateCell<cr>", { desc = "Molten re-eval cell" })
+map("n", "<leader>nM", function() require("nabla").toggle_virt() end, { desc = "Toggle math equations" })
+
+-- todo comments mappings
+map("n", "<leader>tn", function() require("todo-comments").jump_next() end, { desc = "Next Todo Comment" })
+map("n", "<leader>tp", function() require("todo-comments").jump_prev() end, { desc = "Previous Todo Comment" })
+map("n", "<leader>tt", ":TodoTelescope<cr>", { desc = "Todo" })
+map("n", "<leader>ta", ":TodoTelescope keywords=TODO,FIX,FIXME<cr>", { desc = "Todo/Fix/Fixme" })
+
+
 -- normal mode with <leader>
 wk.add({
   {
-    { "<leader><cr>", send_cell,                                       desc = "run code cell" },
-    { "<leader>c",    group = "[c]ode / [c]ell / [c]hunk" },
-    { "<leader>ci",   new_terminal_ipython,                            desc = "new [i]python terminal" },
-    { "<leader>cn",   new_terminal_shell,                              desc = "[n]ew terminal with shell" },
-    { "<leader>cp",   new_terminal_python,                             desc = "new [p]ython terminal" },
-    { "<leader>d",    group = "[d]ebug" },
-    { "<leader>dt",   group = "[t]est" },
-    { "<leader>e",    group = "[e]dit" },
-    { "<leader>f",    group = "[f]ind (telescope)" },
-    { "<leader>fM",   "<cmd>Telescope man_pages<cr>",                  desc = "[M]an pages" },
-    { "<leader>fb",   "<cmd>Telescope current_buffer_fuzzy_find<cr>",  desc = "[b]uffer fuzzy find" },
-    { "<leader>fc",   "<cmd>Telescope git_commits<cr>",                desc = "git [c]ommits" },
-    { "<leader>fd",   "<cmd>Telescope buffers<cr>",                    desc = "[d] buffers" },
-    { "<leader>ff",   "<cmd>Telescope find_files<cr>",                 desc = "[f]iles" },
-    { "<leader>fg",   "<cmd>Telescope live_grep<cr>",                  desc = "[g]rep" },
-    { "<leader>fh",   "<cmd>Telescope help_tags<cr>",                  desc = "[h]elp" },
-    { "<leader>fj",   "<cmd>Telescope jumplist<cr>",                   desc = "[j]umplist" },
-    { "<leader>fk",   "<cmd>Telescope keymaps<cr>",                    desc = "[k]eymaps" },
-    { "<leader>fl",   "<cmd>Telescope loclist<cr>",                    desc = "[l]oclist" },
-    { "<leader>fm",   "<cmd>Telescope marks<cr>",                      desc = "[m]arks" },
-    { "<leader>fq",   "<cmd>Telescope quickfix<cr>",                   desc = "[q]uickfix" },
-    { "<leader>h",    group = "[h]elp / [h]ide / debug" },
-    { "<leader>hc",   group = "[c]onceal" },
-    { "<leader>hch",  ":set conceallevel=1<cr>",                       desc = "[h]ide/conceal" },
-    { "<leader>hcs",  ":set conceallevel=0<cr>",                       desc = "[s]how/unconceal" },
-    { "<leader>ht",   group = "[t]reesitter" },
-    { "<leader>htt",  vim.treesitter.inspect_tree,                     desc = "show [t]ree" },
-    { "<leader>i",    group = "[i]mage" },
-    { "<leader>l",    group = "[l]anguage/lsp" },
-    { "<leader>ld",   group = "[d]iagnostics" },
-    { "<leader>ldd",  function() vim.diagnostic.enable(false) end,     desc = "[d]isable" },
-    { "<leader>lde",  vim.diagnostic.enable,                           desc = "[e]nable" },
-    { "<leader>le",   vim.diagnostic.open_float,                       desc = "diagnostics (show hover [e]rror)" },
-    { "<leader>o",    group = "[o]tter & c[o]de" },
-    { "<leader>oa",   require 'otter'.activate,                        desc = "otter [a]ctivate" },
-    { "<leader>ob",   insert_bash_chunk,                               desc = "[b]ash code chunk" },
-    { "<leader>od",   require 'otter'.activate,                        desc = "otter [d]eactivate" },
-    { "<leader>op",   insert_py_chunk,                                 desc = "[p]ython code chunk" },
-    { "<leader>q",    group = "[q]uarto" },
-    { "<leader>qE",   function() require('otter').export(true) end,    desc = "[E]xport with overwrite" },
-    { "<leader>qa",   ":QuartoActivate<cr>",                           desc = "[a]ctivate" },
-    { "<leader>qe",   require('otter').export,                         desc = "[e]xport" },
-    { "<leader>qh",   ":QuartoHelp ",                                  desc = "[h]elp" },
-    { "<leader>qp",   ":lua require'quarto'.quartoPreview()<cr>",      desc = "[p]review" },
-    { "<leader>qq",   ":lua require'quarto'.quartoClosePreview()<cr>", desc = "[q]uiet preview" },
-    { "<leader>qr",   group = "[r]un" },
-    { "<leader>qra",  ":QuartoSendAll<cr>",                            desc = "run [a]ll" },
-    { "<leader>qrb",  ":QuartoSendBelow<cr>",                          desc = "run [b]elow" },
-    { "<leader>qrr",  ":QuartoSendAbove<cr>",                          desc = "to cu[r]sor" },
-    { "<leader>x",    group = "e[x]ecute" },
+    { "<leader>c",  group = "code/lsp actions" },
+    { "<leader>d",  group = "debug" },
+    { "<leader>dt", group = "test" },
+    { "<leader>e",  group = "edit" },
+    { "<leader>f",  group = "Telescope" },
+    { "<leader>h",  group = "help" },
+    { "<leader>hc", group = "conceal" },
+    { "<leader>i",  group = "image" },
+    { "<leader>cd", group = "diagnostics" },
+    { "<leader>n",  group = "notebook" },
+    { "<leader>nr", group = "run" },
+    { "<leader>t",  group = "Todo" },
+    { "<leader>w",  group = "windows" },
   }
 }, { mode = 'n' })
